817903bbe405c4639d508c6833a1d59bc15d5784
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-01-15 17:15:01 -0500

    feat: seedData folder created to store all of the seeds. currently, the seeds don't run because of ordersData

--- seed.js
+++ seed.js
@@ -1,12 +1,15 @@
 const db = require('./server/db')
 const {User, Record, Order, RecordOrder} = require('./server/db/models')
-const records = require('./records')
+const records = require('./seedData/recordsData')
+const orders = require('./seedData/ordersData')
+const users = require('./seedData/usersData')
 
 const seed = async () => {
   await db.sync({force: true})
   const seedRecords = await Record.bulkCreate(records, {returning: true})
+  const seedOrders = await Order.bulkCreate(orders, {returning: true})
+  const seedUsers = await User.bulkCreate(users, {returning: true})
   console.log('Seeding successful')
-  console.log('Added data:', seedRecords)
 }
 
 seed().catch(err => {
---
+++ seedData/recordOrderData.js
@@ -0,0 +1,47 @@
+const recordOrder = [
+  {
+    quantity: 2,
+    orderId: 1,
+    recordId: 1
+  },
+  {
+    quantity: 1,
+    orderId: 2,
+    recordId: 2
+  },
+  {
+    quantity: 1,
+    orderId: 2,
+    recordId: 5
+  },
+  {
+    quantity: 1,
+    orderId: 3,
+    recordId: 4
+  },
+  {
+    quantity: 1,
+    orderId: 3,
+    recordId: 17
+  },
+  {
+    quantity: 1,
+    orderId: 3,
+    recordId: 19
+  },
+  {
+    quantity: 4,
+    orderId: 4,
+    recordId: 22
+  },
+  {
+    quantity: 1,
+    orderId: 4,
+    recordId: 13
+  },
+  {
+    quantity: 1,
+    orderId: 4,
+    recordId: 18
+  }
+]
