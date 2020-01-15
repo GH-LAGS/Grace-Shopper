9682e281db948be917cea6519dc8b8104347dddc
Gita Linkeviciute <gita.link@gmail.com>
2020-01-15 11:53:15 -0500

    Adds seed file for db

---
+++ records.js
@@ -0,0 +1,67 @@
+const records = [
+  {
+    title: 'Aladdin Sane',
+    artist: 'David Bowie',
+    year: '1973',
+    price: 33,
+    imgURL: './images/bowie.jpg',
+    genre: 'rock',
+    quantity: 10
+  },
+  {
+    title: 'Nevermind',
+    artist: 'Nirvana',
+    year: '1991',
+    price: 29,
+    imgURL: '.images/nirvana.jpg',
+    genre: 'rock',
+    quantity: 10
+  },
+  {
+    title: 'Teenage Dream',
+    artist: 'Katy Perry',
+    year: '2010',
+    price: 29,
+    imgURL: './images/katy.jpg',
+    genre: 'pop',
+    quantity: 10
+  },
+  {
+    title: 'Dookie',
+    artist: 'Green Day',
+    year: '1994',
+    price: 29,
+    imgURL: './images/greenday.jpg',
+    genre: 'indie',
+    quantity: 10
+  },
+  {
+    title: 'Upside Down',
+    artist: 'Kiss',
+    year: '2010',
+    price: 29,
+    imgURL: './images/kiss.jpg',
+    genre: 'rock',
+    quantity: 10
+  },
+  {
+    title: 'Roxy Music',
+    artist: 'Roxy Music',
+    year: '1972',
+    price: 29,
+    imgURL: '.images/roxy.jpg',
+    genre: 'pop',
+    quantity: 10
+  },
+  {
+    title: 'Sgt Pepper’s Lonely Hearts Club Band',
+    artist: 'The Beatles',
+    year: '1967',
+    price: 29,
+    imgURL: '.images/beatles.jpg',
+    genre: 'pop',
+    quantity: 10
+  }
+]
+
+module.exports = records
