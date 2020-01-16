09c24bca5824cab1af525f4675d8c07dcb7a37f6
Shelby Gluck <shelbygluck@gmail.com>
2020-01-16 16:34:20 -0500

    refactor: added backend db and session cart fetch

--- server/api/cart.js
+++ server/api/cart.js
@@ -4,17 +4,24 @@ module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    if (req.user.id) {
-      const userOrders = await Order.findOne({
+    if (req.user) {
+      console.log(req.user)
+      const cart = await Order.findOne({
         where: {
           id: req.user.id,
-          status: pending
+          status: 'pending'
         },
         include: [{model: Record}]
       })
-      res.json(userOrders)
-    } else {
-      console.log('Not logged in')
+      res.json(cart)
+    }
+    if (!req.user) {
+      if (req.session.cart) {
+        res.send(req.session.cart)
+      } else {
+        req.session.cart = {}
+        res.send(req.session.cart)
+      }
     }
   } catch (err) {
     console.log(err)
