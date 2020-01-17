964de72ff3c11ffb8cd69a94b79519c047e6ee9a
Avelina Kim <avelina@gmail.com>
2020-01-16 19:35:09 -0500

    style: adjusted flexbox and added color

--- client/app.js
+++ client/app.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import {Grommet} from 'grommet'
+import {Grommet, grommet} from 'grommet'
 
 import {Navbar} from './components'
 import Routes from './routes'
 
+const customTheme = {
+  global: {
+    colors: {background: '#FCF1B3'}
+  }
+}
+
 const App = () => {
   return (
-    <Grommet plain>
+    <Grommet theme={customTheme}>
       <Navbar />
       <Routes />
     </Grommet>
--- client/components/record.js
+++ client/components/record.js
@@ -5,32 +5,27 @@ import {Grommet, Box, Grid, Text, Button} from 'grommet'
 
 const Record = props => {
   return (
-    <div className="albumContainer">
-      <Box
-        direction="row-responsive"
-        justify="center"
-        align="center"
-        pad="xlarge"
-        background="dark-2"
-        gap="medium"
-        round="small"
-        margin="xlarge"
-      >
-        <input type="image" src={props.record.imgURL} className="albumCover" />
-        <Text direction="row-responsive" justify="center">
-          {props.record.title}
-        </Text>
-        <Text direction="row-responsive" justify="center">
-          {props.record.artist}
-        </Text>
-        <Text direction="row-responsive" justify="center">{`$${props.record
-          .price / 100}`}</Text>
-      </Box>
-
-      <div className="middle">
-        {/* <div className="addToCartText">ADD TO CART</div> */}
-      </div>
-    </div>
+    <Box
+      direction="column"
+      justify="center"
+      align="center"
+      pad="large"
+      background="#5B4037"
+      gap="xsmall"
+      round="small"
+      margin="medium"
+      basis="medium"
+    >
+      <input type="image" src={props.record.imgURL} className="albumCover" />
+      <Text direction="row-responsive" justify="center" weight="">
+        {props.record.title}
+      </Text>
+      <Text direction="row-responsive" justify="center">
+        {props.record.artist}
+      </Text>
+      <Text direction="row-responsive" justify="center">{`$${props.record
+        .price / 100}`}</Text>
+    </Box>
   )
 }
 
