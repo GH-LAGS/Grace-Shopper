f5a3e28b1237be22934ded1a0467352cf64a2e17
Laura Nemeth <lauranemeth.pr@gmail.com>
2020-01-20 14:50:54 -0500

    feat: order history styling

--- client/components/order-history.js
+++ client/components/order-history.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchOrders} from '../store/orders'
 // import Order from './ordered-record'
+import {Grommet, Box, Text, Image} from 'grommet'
 
 export class OrderHistory extends React.Component {
   componentDidMount() {
@@ -12,6 +13,7 @@ export class OrderHistory extends React.Component {
     return (
       <div className="allPastOrders">
         <h1>Past Orders: </h1>
+        <hr />
         <div className="singlePastOrder">
           {this.props.orders.length === 0 ? (
             <h3>No order history yet!</h3>
@@ -20,27 +22,57 @@ export class OrderHistory extends React.Component {
               return (
                 <div key={order.id} order={order}>
                   <h3 id="datePurchased">Purchased on {order.date}</h3>
-                  <h3 id="totalPrice">
-                    Total Price: {`$${order.totalPrice / 100}`}
-                  </h3>
                   {order.Records.map(record => {
                     return (
-                      <div key={record.id}>
-                        <img
-                          src={record.imgURL}
-                          alt={record.title}
-                          height="75"
-                          width="75"
-                        />
-                        <p id="recordTitle">{record.title}</p>
-                        <p id="recordArtist">{record.artist}</p>
-                        <p id="quantity">{record.RecordOrder.quantity}</p>
-                        <p id="priceSold">
-                          Price: {`$${record.RecordOrder.soldPrice / 100}`}
-                        </p>
+                      <div key={record.id} className="OrderHistory">
+                        <Box
+                          direction="row"
+                          justify="between"
+                          align="center"
+                          pad="small"
+                          gap="xsmall"
+                          round="small"
+                          margin="medium"
+                          basis="medium"
+                        >
+                          <Box
+                            pad="medium"
+                            background="#5B4037"
+                            gap="xsmall"
+                            round="small"
+                            margin="medium"
+                          >
+                            <Image
+                              src={record.imgURL}
+                              alt={record.title}
+                              height="200px"
+                              width="200px"
+                            />
+                          </Box>
+                          <Text
+                            direction="row-responsive"
+                            justify="center"
+                            weight="bold"
+                          >
+                            {record.title}
+                          </Text>
+                          <Text direction="row-responsive" justify="center">
+                            {record.artist}
+                          </Text>
+                          <Text direction="row-responsive" justify="center">
+                            {record.RecordOrder.quantity}
+                          </Text>
+                          <Text direction="row-responsive" justify="center">
+                            Price: {`$${record.RecordOrder.soldPrice / 100}`}
+                          </Text>
+                        </Box>
                       </div>
                     )
                   })}
+                  <h3 id="totalPrice">
+                    Total Price: {`$${order.totalPrice / 100}`}
+                  </h3>
+                  <hr />
                 </div>
               )
             })
--- client/components/ordered-record.js
+++ client/components/ordered-record.js
@@ -1,10 +1,14 @@
 // import React from 'react'
+// import {connect} from 'react-redux'
 
-// export default class OrderedRecord extends React.Component {
-//   render() {
-//     return  (
-//     <div className="singleRecord" />
-
-//     )
-//   }
+// export const Order = props => {
+//   return (
+//       <div>
+//       <img src={props.record.imgURL} alt={props.record.title} height="75" width="75" />
+//       <p id="recordTitle">{props.record.title}</p>
+//       <p id="recordArtist">{props.record.artist}</p>
+//       <p id="quantity">{props.record.RecordOrder.quantity}</p>
+//       <p id="priceSold">Price: {`$${props.record.RecordOrder.soldPrice / 100}`}</p>
+//       </div>
+//   )
 // }
