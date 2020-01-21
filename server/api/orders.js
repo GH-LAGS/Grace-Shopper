2907cf279cd9acf8b06dd2b933109f0b47a1eb17
Avelina Kim <avelina@gmail.com>
2020-01-21 14:14:25 -0500

    feature: add checkout order api route

--- server/api/orders.js
+++ server/api/orders.js
@@ -20,3 +20,44 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    let order
+    // User -> change order to complete
+    if (req.user) {
+      order = await Order.update(
+        {status: 'completed'},
+        {
+          where: {
+            userId: req.user.id,
+            status: 'pending'
+          }
+        }
+      )
+    } else {
+      // Guest -> create complete order
+      // move lower to calc total price (need to for both guest & user)
+      order = await Order.create({
+        date: req.date,
+        totalPrice: 0,
+        address: req.address,
+        status: 'complete'
+      })
+      // need to check if RecordOrder exists and quantity++
+      // reduce quantity of Records
+      cart.Records.forEach(record => {
+        RecordOrder.create({
+          orderId: order.id,
+          recordId: record.id,
+          quantity: 1,
+          soldPrice: record.price
+        })
+      })
+      req.session.cart = {Records: []}
+    }
+    res.status(200).send()
+  } catch (err) {
+    next(err)
+  }
+})
